aa45f60fa4ada7ad9cd8c79ed4e3bdf7b5134ab2
mgmyo95 <maungmyo.isn@gmail.com>
2023-08-08 14:49:36 +0630

    M index css js 2:48 8/8/2023 left bar and top bar finished

--- js/app.js
+++ js/app.js
@@ -5,28 +5,28 @@ let getnav = document.querySelector('.navigations');
 let getmain = document.querySelector('.mains');
 let menutitle = document.querySelectorAll('.menu-title');
 
-getbar.addEventListener('click',togglebar);
-function togglebar(){
+getbar.addEventListener('click', togglebar);
+function togglebar() {
     getnav.classList.toggle('active');
     getmain.classList.toggle('active');
 
-    menutitle.forEach(function(title){
-        if(getnav.classList.contains('active')){
+    menutitle.forEach(function (title) {
+        if (getnav.classList.contains('active')) {
             title.style.display = "none";
-        }else{
+        } else {
             title.style.display = "block";
         }
-    }); 
+    });
 }
 
-window.addEventListener('resize',function(){
-    if(this.innerWidth < 998){
-        resizefix();    
+window.addEventListener('resize', function () {
+    if (this.innerWidth < 998) {
+        resizefix();
     }
 });
 
-function resizefix(){
-    if(getnav.classList.contains('active')){
+function resizefix() {
+    if (getnav.classList.contains('active')) {
         togglebar();
     }
 };
@@ -34,50 +34,60 @@ function resizefix(){
 
 
 //show and hide topbar > mega-menu
-let getmegamenu = document.querySelector(".mega-menu");
-let getsubmega = document.querySelector(".sub-mega");
 let geticon = document.querySelector('.right-bar .search .icons');
 let getsearchbox = document.querySelector('.right-bar .search-box');
 let topmenus = document.querySelectorAll('.topmenu');
 let topsub = document.querySelector('.topsub');
 let openfullscreen = document.querySelector('.openfullscreen');
 let closefullscreen = document.querySelector('.closefullscreen');
+let getsettingicon = document.querySelector(".setting-icon");
+let getsettingbox = document.querySelector('.setting-box');
+let getcross = document.querySelector('.cross');
 
-geticon.addEventListener('click',showsearchbox);
-function showsearchbox(){
+geticon.addEventListener('click', showsearchbox);
+function showsearchbox() {
     getsearchbox.classList.toggle('show-search');
 }
 
-topmenus.forEach((topmenu,idx)=>{
-    topmenu.addEventListener('click',function(){
+//topbar icon toggle 
+topmenus.forEach((topmenu, idx) => {
+    topmenu.addEventListener('click', function () {
         // console.log(topmenu,idx);
         showtopmenu(idx);
     });
 });
 
-function showtopmenu(idx){
-    topmenus.forEach((curmenu,curidx)=>{
+function showtopmenu(idx) {
+    topmenus.forEach((curmenu, curidx) => {
         // console.log(idx,curidx);
         let menucontent = curmenu.nextElementSibling;
         // console.log(menucontent);
 
-        if(idx === curidx){
+        if (idx === curidx) {
             // menucontent.classList.add('topshow');
             menucontent.classList.toggle('topshow');
-        }else{
+        } else {
             menucontent.classList.remove('topshow');
         }
     });
 };
 
-openfullscreen.onclick = function(){
+//setting cross toggle
+getcross.addEventListener('click', settingshow);
+function settingshow() {
+    getsettingbox.classList.toggle('topshow');
+}
+
+
+//fullscreen
+openfullscreen.onclick = function () {
     let getde = document.documentElement;
 
-    if(getde.requestFullscreen){
+    if (getde.requestFullscreen) {
         getde.requestFullscreen();
-    }else if(getde.msRequestFullscreen){
+    } else if (getde.msRequestFullscreen) {
         getde.msRequestFullscreen();
-    }else if(getde.webkitRequestFullscreen){
+    } else if (getde.webkitRequestFullscreen) {
         getde.msRequestFullscreen();
     }
 
@@ -86,14 +96,14 @@ openfullscreen.onclick = function(){
 
 };
 
-closefullscreen.onclick = function(){
-    if(document.exitFullscreen){
+closefullscreen.onclick = function () {
+    if (document.exitFullscreen) {
         document.exitFullscreen();
-    }else if(document.mozRequestFullscreen){
+    } else if (document.mozRequestFullscreen) {
         document.mozRequestFullscreen();    //chrome / safari
-    }else if(document.webkitExitFullscreen){
+    } else if (document.webkitExitFullscreen) {
         document.webkitExitFullscreen();
-    }else if(document.msExitFullscreen){
+    } else if (document.msExitFullscreen) {
         document.msExitFullscreen();
     }
 
@@ -102,15 +112,16 @@ closefullscreen.onclick = function(){
 };
 
 
+
 //show and hide topbar > mega-menu
 
 //left sidebar hover animation 
 let navlis = getnav.querySelectorAll('.nav-lists li');
-navlis.forEach(item=>item.addEventListener('mousemove',activelink));
+navlis.forEach(item => item.addEventListener('mousemove', activelink));
 
-function activelink(){
+function activelink() {
     // console.log("hi")
-    navlis.forEach(item=>{
+    navlis.forEach(item => {
         item.classList.remove('hovered');
     });
     this.classList.add('hovered');
